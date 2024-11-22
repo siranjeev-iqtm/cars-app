4c3bf80461ce0830162aaef95f73f496846f9d36
siranjeev-iqtm <siranjeev@iqtechmax.com>
2024-11-22 16:48:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function opennavipanel() {
+    document.getElementById('toggler').classList.toggle('clicked');
+    document.getElementById('mobMenu').classList.toggle('activate');
+}
+
+const carCards = document.querySelectorAll(".car-card");
+carCards.forEach((card) => {
+    card.addEventListener("click", () => {
+        window.location.href = "/cardetails.html";
+    });
+});
